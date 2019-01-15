4a6940a32147979f8e8da650b12ae3b48fa3b635
djbios <dorandval@Gmail.com>
2019-01-15 15:44:10 +0300

    ignored  Please enter the commit message for your changes. Lines starting

--- CartridgeWriterForms/Form1.Designer.cs
+++ CartridgeWriterForms/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.cboPrinterType = new System.Windows.Forms.ComboBox();
+            this.cmdRead = new System.Windows.Forms.Button();
+            this.txtEEPROMUID = new System.Windows.Forms.TextBox();
+            this.txtKeyFragment = new System.Windows.Forms.TextBox();
+            this.cboMaterialCurrent = new System.Windows.Forms.ComboBox();
+            this.cboMaterialChangeTo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // cboDevice
@@ -39,22 +45,85 @@
             this.cboDevice.Size = new System.Drawing.Size(145, 21);
             this.cboDevice.TabIndex = 0;
             // 
+            // cboPrinterType
+            // 
+            this.cboPrinterType.FormattingEnabled = true;
+            this.cboPrinterType.Location = new System.Drawing.Point(163, 12);
+            this.cboPrinterType.Name = "cboPrinterType";
+            this.cboPrinterType.Size = new System.Drawing.Size(84, 21);
+            this.cboPrinterType.TabIndex = 1;
+            // 
+            // cmdRead
+            // 
+            this.cmdRead.Location = new System.Drawing.Point(253, 10);
+            this.cmdRead.Name = "cmdRead";
+            this.cmdRead.Size = new System.Drawing.Size(75, 23);
+            this.cmdRead.TabIndex = 2;
+            this.cmdRead.Text = "Read";
+            this.cmdRead.UseVisualStyleBackColor = true;
+            this.cmdRead.Click += new System.EventHandler(this.cmdRead_Click);
+            // 
+            // txtEEPROMUID
+            // 
+            this.txtEEPROMUID.Enabled = false;
+            this.txtEEPROMUID.Location = new System.Drawing.Point(349, 12);
+            this.txtEEPROMUID.Name = "txtEEPROMUID";
+            this.txtEEPROMUID.Size = new System.Drawing.Size(100, 20);
+            this.txtEEPROMUID.TabIndex = 3;
+            // 
+            // txtKeyFragment
+            // 
+            this.txtKeyFragment.Enabled = false;
+            this.txtKeyFragment.Location = new System.Drawing.Point(468, 12);
+            this.txtKeyFragment.Name = "txtKeyFragment";
+            this.txtKeyFragment.Size = new System.Drawing.Size(100, 20);
+            this.txtKeyFragment.TabIndex = 4;
+            // 
+            // cboMaterialCurrent
+            // 
+            this.cboMaterialCurrent.FormattingEnabled = true;
+            this.cboMaterialCurrent.Location = new System.Drawing.Point(183, 77);
+            this.cboMaterialCurrent.Name = "cboMaterialCurrent";
+            this.cboMaterialCurrent.Size = new System.Drawing.Size(145, 21);
+            this.cboMaterialCurrent.TabIndex = 5;
+            // 
+            // cboMaterialChangeTo
+            // 
+            this.cboMaterialChangeTo.FormattingEnabled = true;
+            this.cboMaterialChangeTo.Location = new System.Drawing.Point(358, 77);
+            this.cboMaterialChangeTo.Name = "cboMaterialChangeTo";
+            this.cboMaterialChangeTo.Size = new System.Drawing.Size(145, 21);
+            this.cboMaterialChangeTo.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 462);
+            this.ClientSize = new System.Drawing.Size(595, 455);
+            this.Controls.Add(this.cboMaterialChangeTo);
+            this.Controls.Add(this.cboMaterialCurrent);
+            this.Controls.Add(this.txtKeyFragment);
+            this.Controls.Add(this.txtEEPROMUID);
+            this.Controls.Add(this.cmdRead);
+            this.Controls.Add(this.cboPrinterType);
             this.Controls.Add(this.cboDevice);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.ComboBox cboPrinterType;
+        private System.Windows.Forms.Button cmdRead;
+        private System.Windows.Forms.TextBox txtEEPROMUID;
+        private System.Windows.Forms.TextBox txtKeyFragment;
+        private System.Windows.Forms.ComboBox cboMaterialCurrent;
+        private System.Windows.Forms.ComboBox cboMaterialChangeTo;
     }
 }
 
--- CartridgeWriterForms/Form1.cs
+++ CartridgeWriterForms/Form1.cs
@@ -23,12 +23,53 @@ namespace CartridgeWriterForms
             InitializeComponent();
 
             cboDevice.Items.AddRange(dm.Devices.ToArray());
-
+            cboPrinterType.Items.AddRange(Machine.GetAllTypes().ToArray());
+            cboMaterialCurrent.Items.AddRange(Material.GetAllNames().ToArray());
+            cboMaterialChangeTo.Items.AddRange(Material.GetAllNames().ToArray());
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void cmdRead_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(cboDevice.Text) || String.IsNullOrEmpty(cboPrinterType.Text))
+            {
+                MessageBox.Show("I need a Device and Printer Type before I can read.");
+                return;
+            }
+
+            c = dm.ReadCartridge(cboDevice.Text, Machine.FromType(cboPrinterType.Text));
+
+            if (c == null)
+                return;
+            LoadControls();
+        }
+
+        private void LoadControls()
+        {
+            //txtEEPROMUID.Text = c.EEPROMUID.HexString();
+            //txtKeyFragment.Text = c.KeyFragment;
+            //txtSerialNumberCurrent.Text = c.SerialNumber.ToString("f1");
+            //txtSerialNumberChangeTo.Text = txtSerialNumberCurrent.Text;
+            //cboMaterialCurrent.Text = c.Material.Name;
+            //cboMaterialChangeTo.Text = cboMaterialCurrent.Text;
+            //txtManufacturingLotCurrent.Text = c.ManufacturingLot;
+            //txtManufacturingLotChangeTo.Text = txtManufacturingLotCurrent.Text;
+            //txtManufacturingDateCurrent.Text = c.ManfuacturingDate.ToString("yyyy'-'MM'-'dd HH':'mm':'ss");
+            //txtManufacturingDateChangeTo.Text = txtManufacturingDateCurrent.Text;
+            //txtLastUseDateCurrent.Text = c.UseDate.ToString("yyyy'-'MM'-'dd HH':'mm':'ss");
+            //txtLastUseDateChangeTo.Text = txtLastUseDateCurrent.Text;
+            //txtInitialQuantityCurrent.Text = c.InitialMaterialQuantity.ToString();
+            //txtInitialQuantityChangeTo.Text = txtInitialQuantityCurrent.Text;
+            //txtCurrentQuantityCurrent.Text = c.CurrentMaterialQuantity.ToString();
+            //txtCurrentQuantityChangeTo.Text = txtCurrentQuantityCurrent.Text;
+            //txtVersionCurrent.Text = c.Version.ToString();
+            //txtVersionChangeTo.Text = txtVersionCurrent.Text;
+            //txtSignatureCurrent.Text = c.Signature;
+            //txtSignatureChangeTo.Text = txtSignatureCurrent.Text;
         }
     }
 }
